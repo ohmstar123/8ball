df4ad88aeea4d561b9dc71056e213f260705ea93
ohmstar123 <ohmpate114@ed.amdsb.ca>
2018-10-22 14:53:51 -0400

    need help soon

--- 8ball/Form1.Designer.cs
+++ 8ball/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ballButton
+            // 
+            this.ballButton.FlatAppearance.BorderSize = 0;
+            this.ballButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ballButton.Image = global::_8ball.Properties.Resources.clickBall;
+            this.ballButton.Location = new System.Drawing.Point(143, 12);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(538, 441);
+            this.ballButton.TabIndex = 0;
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(12, 411);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.ballButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ballButton;
+        private System.Windows.Forms.Label outputLabel;
     }
 }
 
--- 8ball/Form1.cs
+++ 8ball/Form1.cs
@@ -12,9 +12,48 @@ namespace _8ball
 {
     public partial class Form1 : Form
     {
+        int ballNumber;
+
         public Form1()
         {
             InitializeComponent();
+            Random randgen = new Random();
+            ballNumber = randgen.Next(1, 7);
+        }
+
+        private void ballButton_Click(object sender, EventArgs e)
+        {
+
+            switch (ballNumber)
+            {
+                case 1:
+                    outputLabel.Text = "Sure";
+                    break;
+
+                case 2:
+                    outputLabel.Text = "Maybe";
+                    break;
+
+                case 3:
+                    outputLabel.Text = "I'm not sure";
+                    break;
+
+                case 4:
+                    outputLabel.Text = "Why not";
+                    break;
+
+                case 5:
+                    outputLabel.Text = "Nope";
+                    break;
+
+                case 6:
+                    outputLabel.Text = "What do you think";
+                    break;
+
+
+
+            }
+
         }
     }
 }
